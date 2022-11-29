5f325c96f55c9c27b20621890b49253a92e6655c
Yash Patel <yashpatel@Yashs-MacBook-Pro.local>
2022-11-29 15:04:45 +0530

    dapp index.html

--- november-29-2022/contract.sol
+++ november-29-2022/contract.sol
@@ -1,31 +1,46 @@
 //SPDX-License-Identifier:MIT
 // Yash Patel BE COMPS 2019130047
-pragma solidity ^ 0.8.0;
+pragma solidity ^0.8.0;
 
 contract YashPatelzDapp {
     string name;
     string email;
-    uint contact;
+    uint256 contact;
     string college;
     address owner;
-    
+
     constructor() {
         owner = msg.sender;
     }
 
-    modifier onlyOwner {
-        require (msg.sender == owner, "Only owner can retrieve data");
-         _;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can retrieve data");
+        _;
     }
 
-    function set (string memory _name, string memory _email, uint256 _contact, string memory _college) public {
+    function set(
+        string memory _name,
+        string memory _email,
+        uint256 _contact,
+        string memory _college
+    ) public {
         name = _name;
         email = _email;
         contact = _contact;
         college = _college;
     }
 
-    function retrieve () public view  onlyOwner returns(string memory, string memory, uint256, string memory) {
+    function retrieve()
+        public
+        view
+        onlyOwner
+        returns (
+            string memory,
+            string memory,
+            uint256,
+            string memory
+        )
+    {
         return (name, email, contact, college);
     }
 }
